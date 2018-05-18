e957df38179450fd895d14350253775aaa45005a
Cyrille Perois <cperois@outlook.com>
2018-05-18 22:10:33 +0200

    chore: bump @phenomic/* dependencies to 1.0.0-beta.2 (#44)

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { Router, Route, browserHistory } from "react-router";
-import {
-    createApp,
-    renderApp,
-} from "@phenomic/preset-react-app/lib/client";
+import { createApp, renderApp } from "@phenomic/preset-react-app/lib/client";
 import "./css/main.css";
-import "highlight.js/styles/atom-one-dark.css";
 import { HomeContainer } from "./components/Home";
 import { PostContainer } from "./components/Post";
 import { ErrorPage } from "./components/ErrorPage";
 
 const routes = () => (
-    <Router history={browserHistory}>
-        <Route path="/" component={HomeContainer} />
-        <Route path="/article/*" component={PostContainer} />
-        <Route path="*" component={ErrorPage} />
-    </Router>
+  <Router history={browserHistory}>
+    <Route path="/" component={HomeContainer} />
+    <Route path="/article/*" component={PostContainer} />
+    <Route path="*" component={ErrorPage} />
+  </Router>
 );
 
 export default createApp(routes);
 
 if (module.hot) {
-    module.hot.accept(() => renderApp(routes));
+  module.hot.accept(() => renderApp(routes));
 }
