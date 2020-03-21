1035fe4d33ca16e5d612733f6fd25f2a127cde36
Cyrille Perois <cperois@protonmail.com>
2020-03-21 15:36:46 +0100

    feat: use global Layout via wrapPageElement gatsby API

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,54 @@
 import React from "react"
-import { Layout } from "../components/Layout"
 
 function IndexPage() {
-  return <Layout></Layout>
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        vulputate neque euismod nibh dapibus imperdiet. Vivamus pellentesque
+        nulla non felis convallis porta. Nullam scelerisque nec risus ut tempor.
+        Praesent iaculis velit at tristique blandit. Mauris faucibus est est, at
+        commodo enim eleifend sed. Proin eget lobortis eros, at ullamcorper
+        lectus. Quisque vulputate lacus ut est fringilla, eget posuere ante
+        pellentesque. Sed lectus leo, lobortis quis lorem ullamcorper, volutpat
+        elementum dolor. Curabitur a facilisis est. Maecenas sagittis
+        ullamcorper sodales. Maecenas a massa urna.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        vulputate neque euismod nibh dapibus imperdiet. Vivamus pellentesque
+        nulla non felis convallis porta. Nullam scelerisque nec risus ut tempor.
+        Praesent iaculis velit at tristique blandit. Mauris faucibus est est, at
+        commodo enim eleifend sed. Proin eget lobortis eros, at ullamcorper
+        lectus. Quisque vulputate lacus ut est fringilla, eget posuere ante
+        pellentesque. Sed lectus leo, lobortis quis lorem ullamcorper, volutpat
+        elementum dolor. Curabitur a facilisis est. Maecenas sagittis
+        ullamcorper sodales. Maecenas a massa urna.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        vulputate neque euismod nibh dapibus imperdiet. Vivamus pellentesque
+        nulla non felis convallis porta. Nullam scelerisque nec risus ut tempor.
+        Praesent iaculis velit at tristique blandit. Mauris faucibus est est, at
+        commodo enim eleifend sed. Proin eget lobortis eros, at ullamcorper
+        lectus. Quisque vulputate lacus ut est fringilla, eget posuere ante
+        pellentesque. Sed lectus leo, lobortis quis lorem ullamcorper, volutpat
+        elementum dolor. Curabitur a facilisis est. Maecenas sagittis
+        ullamcorper sodales. Maecenas a massa urna.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        vulputate neque euismod nibh dapibus imperdiet. Vivamus pellentesque
+        nulla non felis convallis porta. Nullam scelerisque nec risus ut tempor.
+        Praesent iaculis velit at tristique blandit. Mauris faucibus est est, at
+        commodo enim eleifend sed. Proin eget lobortis eros, at ullamcorper
+        lectus. Quisque vulputate lacus ut est fringilla, eget posuere ante
+        pellentesque. Sed lectus leo, lobortis quis lorem ullamcorper, volutpat
+        elementum dolor. Curabitur a facilisis est. Maecenas sagittis
+        ullamcorper sodales. Maecenas a massa urna.
+      </p>
+    </>
+  )
 }
 
 export default IndexPage
